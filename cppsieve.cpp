022845c19ce82af9c374725e78c23a1a520f9962
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-11-14 17:30:10 +0530

    Update cppsieve.cpp

--- cppsieve.cpp
+++ cppsieve.cpp
@@ -1,12 +1,45 @@
-#include <chrono>
 #include <iostream>
 #include <algorithm>
 #include <cmath>
 #include <vector>
 #include <cstdlib>
 #include <stdint.h>
-using namespace std::chrono;
-const int64_t L1D_CACHE_SIZE = 25000000;
+
+/// Set your CPU's L1 data cache size (in bytes) here
+const int64_t L1D_CACHE_SIZE = 32768;
+
+/// Bitmasks needed to unset bits corresponding to multiples
+const int64_t unset_bit[16] =
+{
+  ~(1 << 0), ~(1 << 0),
+  ~(1 << 1), ~(1 << 1),
+  ~(1 << 2), ~(1 << 2),
+  ~(1 << 3), ~(1 << 3),
+  ~(1 << 4), ~(1 << 4),
+  ~(1 << 5), ~(1 << 5),
+  ~(1 << 6), ~(1 << 6),
+  ~(1 << 7), ~(1 << 7)
+};
+
+const int64_t popcnt[256] =
+{
+  0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+  1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+  1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+  2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+  1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+  2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+  2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+  3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+  1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+  2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+  2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+  3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+  2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+  3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+  3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+  4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8
+};
 
 /// Generate primes using the segmented sieve of Eratosthenes.
 /// This algorithm uses O(n log log n) operations and O(sqrt(n)) space.
@@ -15,26 +48,28 @@ const int64_t L1D_CACHE_SIZE = 25000000;
 void segmented_sieve(int64_t limit)
 {
   int64_t sqrt = (int64_t) std::sqrt(limit);
-  int64_t segment_size = std::max(sqrt, L1D_CACHE_SIZE);
-  int64_t count = (limit < 2) ? 0 : 1;
+  int64_t sieve_size = std::max(sqrt, L1D_CACHE_SIZE);
+  int64_t segment_size = sieve_size * 16;
+  int64_t count = (limit == 1) ? -1 : 0;
 
   // we sieve primes >= 3
   int64_t i = 3;
-  int64_t n = 3;
   int64_t s = 3;
 
-  std::vector<char> sieve(segment_size);
-  std::vector<char> is_prime(sqrt + 1, true);
+  // vector used for sieving
+  std::vector<uint8_t> sieve(sieve_size);
+  std::vector<uint8_t> is_prime(sqrt + 1, true);
   std::vector<int64_t> primes;
   std::vector<int64_t> multiples;
 
   for (int64_t low = 0; low <= limit; low += segment_size)
   {
-    std::fill(sieve.begin(), sieve.end(), true);
+    std::fill(sieve.begin(), sieve.end(), 0xff);
 
     // current segment = [low, high]
     int64_t high = low + segment_size - 1;
     high = std::min(high, limit);
+    sieve_size = (high - low) / 16 + 1;
 
     // generate sieving primes using simple sieve of Eratosthenes
     for (; i * i <= high; i += 2)
@@ -52,18 +87,25 @@ void segmented_sieve(int64_t limit)
       }
     }
 
-    // sieve the current segment
+    // sieve segment using bit array
     for (std::size_t i = 0; i < primes.size(); i++)
     {
       int64_t j = multiples[i];
       for (int64_t k = primes[i] * 2; j < segment_size; j += k)
-        sieve[j] = false;
+        sieve[j >> 4] &= unset_bit[j & 15];
       multiples[i] = j - segment_size;
     }
 
-    for (; n <= high; n += 2)
-      if (sieve[n - low]) // n is a prime
-        count++;
+    // unset bits > limit
+    if (high == limit)
+    {
+      int64_t bits = 0xff << (limit % 16 + 1) / 2;
+      sieve[sieve_size - 1] &= ~bits;
+    }
+
+    // count primes
+    for (int64_t n = 0; n < sieve_size; n++)
+      count += popcnt[sieve[n]];
   }
 
   std::cout << count << " primes found." << std::endl;
@@ -74,13 +116,10 @@ void segmented_sieve(int64_t limit)
 ///
 int main(int argc, char** argv)
 {
-  auto start = high_resolution_clock::now();
   if (argc >= 2)
     segmented_sieve(std::atoll(argv[1]));
   else
     segmented_sieve(1000000000);
-  auto stop = high_resolution_clock::now();
-  auto duration = duration_cast<microseconds>(stop - start);
-  std::cout << duration.count() << std::endl;
+
   return 0;
 }
